9c75e00e764597878730db86a085f11441a05d5d
user <user@example.com>
2026-02-06 14:04:11 +0530

    Initial commit

---
+++ src/test/java/com/unstay/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unstay.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
